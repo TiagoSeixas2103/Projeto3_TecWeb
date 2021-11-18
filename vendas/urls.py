a08c6254d55b25b6be53fd5f159a567293a12489
Tiago Seixas <tvseixas@gmail.com>
2021-11-18 17:20:57 -0300

    setando o basico

--- vendas/urls.py
+++ vendas/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post', views.index, name='index'),
+    path('delete', views.delete, name='delete'),
+    path('update', views.update, name='update'),
 ]
--- vendas/views.py
+++ vendas/views.py
@@ -5,12 +5,38 @@ from .models import Venda
 def index(request):
     if request.method == 'POST':
         title = request.POST.get('titulo')
-        content = request.POST.get('detalhes')
-        price = request.POST.get('preço')
-        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+        content = request.POST.get('content')
+        price = request.POST.get('price')
+        # TAREFA: Utilize o title e content para criar um novo Venda no banco de dados
         film = Venda(title = title, content = content, price = price)
         film.save()
         return redirect('index')
     else:
         all_vendas = Venda.objects.all()
         return render(request, 'vendas/index.html', {'vendas': all_vendas})
+
+def delete(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        Venda.objects.filter(id=id).delete()
+        return redirect('index')
+    else:
+        all_vendas = Venda.objects.all()
+        return render(request, 'Vendas/index.html', {'vendas': all_vendas})
+
+def update(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        title = request.POST.get('titulo')
+        content = request.POST.get('content')
+        price = request.POST.get('price')
+        new_venda = Venda()
+        new_venda.title = title
+        new_venda.content = content
+        new_venda.price = price
+        new_venda.id = id
+        new_venda.save()
+        return redirect('index')
+    else:
+        all_vendas = Venda.objects.all()
+        return render(request, 'Vendas/index.html', {'vendas': all_vendas})
